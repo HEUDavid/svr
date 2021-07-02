9f19fd80aa3eda6797272a4019b075ea9555148b
davidxiang <davidxiang@futunn.com>
2021-07-02 17:02:00 +0800

    upt

--- init.sh
+++ init.sh
@@ -7,18 +7,19 @@ dnf install docker-ce -y
 # systemctl disable firewalld
 systemctl enable --now docker
 
-
 echo "install pkg"
 dnf install git -y
 
-echo "nginx"
 git clone https://github.com/HEUDavid/svr.git
-docker run -itd --name nginx -p 80:80 -p 443:443 -v /root/svr/nginx/conf.d:/etc/nginx/conf.d -v /root/svr/nginx/log:/var/log/nginx -v /root/svr/nginx/html:/usr/share/nginx/html nginx
 
-echo "wordpress"
-docker run -itd --name mdavid.cn -p 7080:80 -v /root/svr/www/mdavid.cn:/var/www/html/ wordpress
+echo "nginx"
+docker run -itd --name nginx -p 80:80 -p 443:443 -v /root/svr/nginx/conf.d:/etc/nginx/conf.d -v /root/svr/nginx/log:/var/log/nginx -v /root/svr/nginx/html:/usr/share/nginx/html nginx
 
 echo "db"
 docker run -itd --name mysql -p 4306:3306 -v /root/svr/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 mariadb
+
+echo "wordpress"
+docker run -itd --name mdavid.cn -p 7080:80 -v /root/svr/wordpress/mdavid.cn:/var/wordpress/html/ wordpress
+
 
 
