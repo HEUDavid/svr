e28ac2b9508406a65645b76aceb008832bcb9e49
David Xiang <admin@mdavid.cn>
2023-12-25 00:08:27 +0800

    upt

--- nginx/html/src/script.js
+++ nginx/html/src/script.js
@@ -1,42 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    async function fetchData() {
-        try {
-            const response = await fetch('/data');
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
-            }
-            const files = await response.json();
-            handleFileList(files);
-        } catch (error) {
-            console.error('Fetch error:', error.message);
-        }
-    }
+    // 模拟从服务器获取数据
+    const simulatedFiles = [
+        {name: 'file1.txt', mtime: '2023-01-01T12:00:00Z'},
+        {name: 'file2.txt', mtime: '2023-01-02T09:30:00Z'},
+    ];
 
-    function createFileListItem(file) {
-        const li = document.createElement('li');
-
-        const span = document.createElement('span');
-        span.className = 'circle';
-        li.appendChild(span);
-
-        const link = document.createElement('a');
-        link.target = '_blank';
-        link.href = `/data/${file.name}`;
-
-        const date = new Date(file.mtime);
-        link.textContent = `${date.toLocaleString()} ${file.name}`;
-
-        li.appendChild(link);
-        return li;
-    }
-
-    function handleFileList(files) {
-        const fileListElement = document.getElementById('fileList');
-        files.forEach(file => {
-            const li = createFileListItem(file);
-            fileListElement.appendChild(li);
-        });
-    }
-
-    fetchData();
+    // 模拟处理文件列表
+    handleFileList(simulatedFiles);
 });
+
+function createFileListItem(file) {
+    const li = document.createElement('li');
+
+    const span = document.createElement('span');
+    span.className = 'circle';
+    li.appendChild(span);
+
+    const link = document.createElement('a');
+    link.target = '_blank';
+    link.href = `/data/${file.name}`;
+
+    const date = new Date(file.mtime);
+    link.textContent = `${date.toLocaleString()} ${file.name}`;
+
+    li.appendChild(link);
+    return li;
+}
+
+function handleFileList(files) {
+    const fileListElement = document.getElementById('fileList');
+    files.forEach(file => {
+        const li = createFileListItem(file);
+        fileListElement.appendChild(li);
+    });
+}
