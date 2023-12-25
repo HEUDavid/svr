cf7f28e17871dd5f8df4fec2e17ec3669f9d42c3
David Xiang <admin@mdavid.cn>
2023-12-26 02:16:20 +0800

    upt

--- nginx/conf.d/lua/api.lua
+++ nginx/conf.d/lua/api.lua
@@ -1,31 +1,6 @@
 -- api.lua
 
-local cjson = require("cjson")
-local lfs = require("lfs")
-
-local main = {}
-
-function main.get_files(path)
-    local files = {}
-
-    for file in lfs.dir(path) do
-        if file ~= "." and file ~= ".." then
-            local attr = lfs.attributes(path .. file)
-            table.insert(files, {
-                name = file,
-                size = attr.size,
-                mtime = attr.modification
-            })
-        end
-    end
-    return cjson.encode(files)
-end
-
-function main.say_hello()
-    return "hello from mdavid.cn"
-end
-
-
+local main = require("lua.main")
 
 -- dispatcher
 local uri = ngx.var.uri
