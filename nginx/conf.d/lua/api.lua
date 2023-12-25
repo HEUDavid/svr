7d9f8678f9a908a3a6c2d3ed4937acec8c421239
David Xiang <admin@mdavid.cn>
2023-12-25 23:16:27 +0800

    upt

--- nginx/conf.d/lua/api.lua
+++ nginx/conf.d/lua/api.lua
@@ -1,42 +1,6 @@
 -- api.lua
 
-local cjson = require("cjson")
-local lfs = require("lfs")
+local main = require("main")
 
-local function get_pages(directory)
-    local data = {}
-    for file in lfs.dir(directory) do
-        if file ~= "." and file ~= ".." then
-            local path = directory .. "/" .. file
-            local attr = lfs.attributes(path)
-
-            if attr and attr.mode == "file" then
-                table.insert(data, { name = file, mtime = attr.modification })
-            end
-        end
-    end
-
-    return cjson.encode(data)
-end
-
-local function say_hello()
-    return "hello from mdavid.cn"
-end
-
-function dispatcher()
-    local uri = ngx.var.uri
-
-    if uri == "/api" then
-        ngx.header.content_type = "application/json"
-        ngx.say(get_pages())
-
-    elseif uri == "/api/hello" then
-        ngx.header.content_type = "text/plain"
-        ngx.say(say_hello())
-
-    else
-        ngx.exit(ngx.HTTP_NOT_FOUND)
-    end
-end
-
-dispatcher()
+local uri = ngx.var.uri
+main.handle_request(uri)
