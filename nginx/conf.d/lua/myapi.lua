50200620b9ec9f8e763f2eb182a7ee2e4e919b63
David Xiang <admin@mdavid.cn>
2023-12-25 21:44:35 +0800

    upt

--- nginx/conf.d/lua/myapi.lua
+++ nginx/conf.d/lua/myapi.lua
@@ -1,15 +1,3 @@
 -- myapi.lua
 
-local cjson = require "cjson"
-
-local _M = {}
-
-function _M.handle_request()
-    local response = {
-        status = "success",
-        message = "Hello, this is your API response from Lua script!"
-    }
-    ngx.say(cjson.encode(response))
-end
-
-return _M
+ngx.say("Hello")
