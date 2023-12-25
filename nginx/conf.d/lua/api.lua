860d25f3103487d4ccf8bd536f7ceb2b66e61722
David Xiang <admin@mdavid.cn>
2023-12-25 23:29:38 +0800

    upt

--- nginx/conf.d/lua/api.lua
+++ nginx/conf.d/lua/api.lua
@@ -23,7 +23,16 @@ function api.say_hello()
     return "hello from mdavid.cn"
 end
 
---print(api.say_hello())
---print(api.get_pages("./"))
+local uri = ngx.var.uri
 
-return api
+if uri == "/api" then
+    ngx.header.content_type = "application/json"
+    ngx.say(api.get_pages("/data"))
+
+elseif uri == "/api/hello" then
+    ngx.header.content_type = "text/plain"
+    ngx.say(api.say_hello())
+
+else
+    ngx.exit(ngx.HTTP_NOT_FOUND)
+end
