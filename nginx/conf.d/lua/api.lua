0304030cbb471c11290dd72a25d4e1a5b3cf8d84
David Xiang <admin@mdavid.cn>
2024-08-02 09:06:21 +0800

    upt

--- nginx/conf.d/lua/api.lua
+++ nginx/conf.d/lua/api.lua
@@ -24,23 +24,29 @@ function M.get_files(path)
 end
 
 function M.say_hello()
-    return "hello from mdavid.cn"
+    local cmd = "python3 get_node.py"
+    os.execute(cmd)
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+    return result
 end
 
+local debug = true
 
--- dispatcher
-local uri = ngx.var.uri
-
-if uri == "/api" then
-    ngx.header.content_type = "text/plain"
-    ngx.say(M.say_hello())
-
-
-elseif uri == "/api/get_files" then
-    ngx.header.content_type = "application/json"
-    ngx.say(M.get_files("/data/"))
-
+if debug == false then
+    -- dispatcher
+    local uri = ngx.var.uri
+    if uri == "/api" then
+        ngx.header.content_type = "text/plain"
+        ngx.say(M.say_hello())
+    elseif uri == "/api/get_files" then
+        ngx.header.content_type = "application/json"
+        ngx.say(M.get_files("/data/"))
+    else
+        ngx.exit(ngx.HTTP_NOT_FOUND)
+    end
 
 else
-    ngx.exit(ngx.HTTP_NOT_FOUND)
+    M.say_hello()
 end
