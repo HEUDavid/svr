bd22d8cf7c958f21e289182349ddea318dd52ed4
David Xiang <admin@mdavid.cn>
2024-08-05 00:07:30 +0800

    upt

--- v2ray/clashV/entry.js
+++ v2ray/clashV/entry.js
@@ -1,84 +1,81 @@
-const groupBaseOption = {
-  "interval": 300,
-  "timeout": 3000,
+const myProxy = {
+  "name": "mDavid_CN",
+  "type": "vmess",
+  "uuid": "abc",
+  "server": "ip",
+  "port": 10086,
+  "alterId": 0,
+  "cipher": "none",
+  "udp": true
+};
+const groupBase = {
+  "interval": 300, // 单位s
+  "timeout": 5, // 单位s
   "url": "https://www.google.com/generate_204",
   "lazy": true,
   "max-failed-times": 3,
   "hidden": false
 };
-const myProxy = {
-  "name": "mDavid_CN",
-  "type": "vmess",
-  "server": "ip",
-  "port": 10086,
-  "uuid": "abc",
-  "alterId": 0,
-  "cipher": "none",
-  "udp": true
-};
-const good = {
-  ...groupBaseOption,
+const goodGroup = {
   "name": "GOOD",
   "type": "select",
   "proxies": [myProxy.name],
-  // "include-all": true,
-  // "filter": ".*mDavid.*",
-  "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg"
+  "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg",
+  ...groupBase
 };
-const sub = {
+const subGroup = {
   "name": "SUB",
   "type": "url-test",
-  "tolerance": 50,
   "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/link.svg",
-  ...groupBaseOption,
-  "proxies": ["DIRECT"],
+  ...groupBase,
+  "proxies": [],
   "use": []
 };
+const proxyGroups = [
+  {
+    "name": "PROXY",
+    "type": "select",
+    "proxies": ["GOOD", "SUB"],
+    "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/adjust.svg",
+    ...groupBase
+  },
+  {
+    "name": "MEDIA",
+    "type": "select",
+    "proxies": ["GOOD", "SUB"],
+    "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/youtube.svg",
+    ...groupBase
+  },
+  {}, // 自建节点GOOD分组占位
+  {}, // 订阅节点SUB分组占位
+  {
+    "name": "FINAL",
+    "type": "select",
+    "proxies": ["GOOD", "SUB", "DIRECT"],
+    "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/fish.svg",
+    ...groupBase
+  },
+];
 function main(config, profileName) {
-  console.log(profileName);
+  console.log("load: " + profileName);
   const proxyCount = config?.proxies?.length ?? 0;
-  console.log(proxyCount);
   const proxyProviderCount =
     typeof config?.["proxy-providers"] === "object" ? Object.keys(config["proxy-providers"]).length : 0;
+  console.log("proxy: " + proxyCount + ", proxyProvider: " + proxyProviderCount);
   if (proxyCount === 0 && proxyProviderCount === 0) {
     throw new Error("配置文件中未找到任何代理");
   }
-  console.log(proxyProviderCount);
   if (proxyCount > 0) {
-    sub.proxies = config.proxies.map((proxy) => proxy.name);
+    subGroup.proxies = config.proxies.map((proxy) => proxy.name);
     config.proxies.unshift(myProxy);
   } else {
-    config.proxies = [
-      myProxy,
-    ];
+    config.proxies = [myProxy];
   }
   if (proxyProviderCount > 0) {
-    sub.use = Object.keys(config["proxy-providers"]);
+    subGroup.use = Object.keys(config["proxy-providers"]);
   }
-  config["proxy-groups"] = [
-    {
-      "name": "PROXY",
-      "type": "select",
-      "proxies": ["GOOD", "SUB"],
-      "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/adjust.svg",
-      ...groupBaseOption,
-    },
-    {
-      "name": "MEDIA",
-      "type": "select",
-      "proxies": ["GOOD", "SUB"],
-      "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/youtube.svg",
-      ...groupBaseOption,
-    },
-    good,
-    sub,
-    {
-      "name": "FINAL",
-      "type": "select",
-      "proxies": ["GOOD", "SUB", "DIRECT"],
-      "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/fish.svg",
-      ...groupBaseOption,
-    },
-  ];
+  proxyGroups[2] = goodGroup
+  proxyGroups[3] = subGroup
+  config["proxy-groups"] = proxyGroups;
   return config;
 }
