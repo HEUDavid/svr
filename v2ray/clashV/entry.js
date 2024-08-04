50644bbfe1aeb18aef0957a377d48c247dab78df
David Xiang <admin@mdavid.cn>
2024-08-05 04:14:01 +0800

    upt

--- v2ray/clashV/entry.js
+++ v2ray/clashV/entry.js
@@ -1,13 +1,3 @@
-const myProxy = {
-  name: "mDavid_CN",
-  type: "vmess",
-  uuid: "abc",
-  server: "ip",
-  port: 10086,
-  alterId: 0,
-  cipher: "none",
-  udp: true,
-};
 const groupBase = {
   interval: 300, // 单位s
   timeout: 5, // 单位s
@@ -15,13 +5,6 @@ const groupBase = {
   lazy: true,
   "max-failed-times": 3,
   hidden: false,
-};
-const goodGroup = {
-  name: "GOOD",
-  type: "select",
-  proxies: [myProxy.name],
-  icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg",
-  ...groupBase,
 };
 const subGroup = {
   name: "SUB",
@@ -46,7 +29,13 @@ const proxyGroups = [
     icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/youtube.svg",
     ...groupBase,
   },
-  {}, // 自建节点GOOD分组占位
+  {
+    name: "GOOD",
+    type: "select",
+    proxies: ["mDavid_CN"],
+    icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg",
+    ...groupBase,
+  },
   {}, // 订阅节点SUB分组占位
   {
     name: "FINAL",
@@ -56,34 +45,24 @@ const proxyGroups = [
     ...groupBase,
   },
 ];
-const ruleSets = ["mdavid_proxy", "mdavid_direct", "douyin", "applications"];
 function main(config, profileName) {
-  console.log("load: " + profileName);
   const proxyCount = config?.proxies?.length ?? 0;
   const proxyProviderCount =
     typeof config?.["proxy-providers"] === "object"
       ? Object.keys(config["proxy-providers"]).length
       : 0;
-  console.log(
-    "proxy: " + proxyCount + ", proxyProvider: " + proxyProviderCount
-  );
-  if (proxyCount === 0 && proxyProviderCount === 0) {
-    throw new Error("配置文件中未找到任何代理");
-  }
   if (proxyCount > 0) {
-    subGroup.proxies = config.proxies.map((proxy) => proxy.name);
-    config.proxies.unshift(myProxy);
-  } else {
-    config.proxies = [myProxy];
+    subGroup.proxies = config.proxies
+      .map((proxy) => proxy.name)
+      .filter((name) => !name.includes("mDavid"));
   }
   if (proxyProviderCount > 0) {
     subGroup.use = Object.keys(config["proxy-providers"]);
   }
-  proxyGroups[2] = goodGroup;
   proxyGroups[3] = subGroup;
   config["proxy-groups"] = proxyGroups;
   config["rule-providers"] = Object.keys(config["rule-providers"])
-    .filter((key) => ruleSets.includes(key))
+    .filter((key) => config["used-rule-providers"].includes(key))
     .reduce((obj, key) => {
       obj[key] = config["rule-providers"][key];
       return obj;
