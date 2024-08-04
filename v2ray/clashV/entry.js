193988dd0f60d0db806fe3c1fea1aaa5243f5fe2
David Xiang <admin@mdavid.cn>
2024-08-04 23:28:06 +0800

    upt

--- v2ray/clashV/entry.js
+++ v2ray/clashV/entry.js
@@ -6,24 +6,51 @@ const groupBaseOption = {
   "max-failed-times": 3,
   "hidden": false
 };
+const myProxy = {
+  "name": "mDavid_CN",
+  "type": "vmess",
+  "server": "ip",
+  "port": 10086,
+  "uuid": "abc",
+  "alterId": 0,
+  "cipher": "none",
+  "udp": true
+};
+const good = {
+  ...groupBaseOption,
+  "name": "GOOD",
+  "type": "select",
+  "proxies": [myProxy.name],
+  // "include-all": true,
+  // "filter": ".*mDavid.*",
+  "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg"
+};
+const sub = {
+  "name": "SUB",
+  "type": "url-test",
+  "tolerance": 50,
+  "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/link.svg",
+  ...groupBaseOption,
+  "proxies": ["DIRECT"],
+  "use": []
+};
 function main(config, profileName) {
+  console.log(profileName);
   const proxyCount = config?.proxies?.length ?? 0;
+  console.log(proxyCount);
   const proxyProviderCount =
     typeof config?.["proxy-providers"] === "object" ? Object.keys(config["proxy-providers"]).length : 0;
   if (proxyCount === 0 && proxyProviderCount === 0) {
     throw new Error("配置文件中未找到任何代理");
   }
-  const sub = {
-    "name": "SUB",
-    "type": "url-test",
-    "tolerance": 50,
-    "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/link.svg",
-    ...groupBaseOption,
-  };
+  console.log(proxyProviderCount);
   if (proxyCount > 0) {
-    sub.proxies = config.proxies
-      .map((proxy) => proxy.name)
-      .filter((name) => !name.includes("mDavid"));
+    sub.proxies = config.proxies.map((proxy) => proxy.name);
+    config.proxies.unshift(myProxy);
+  } else {
+    config.proxies = [
+      myProxy,
+    ];
   }
   if (proxyProviderCount > 0) {
     sub.use = Object.keys(config["proxy-providers"]);
@@ -43,14 +70,7 @@ function main(config, profileName) {
       "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/youtube.svg",
       ...groupBaseOption,
     },
-    {
-      ...groupBaseOption,
-      "name": "GOOD",
-      "type": "select",
-      "include-all": true,
-      "filter": ".*mDavid.*",
-      "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg"
-    },
+    good,
     sub,
     {
       "name": "FINAL",
