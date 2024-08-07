a5a2cda653d3126a2b49fcc59e6a0eee3d285373
David Xiang <admin@mdavid.cn>
2024-08-08 01:47:50 +0800

    upt

--- v2ray/clashV/entry.js
+++ v2ray/clashV/entry.js
@@ -45,12 +45,25 @@ const allGroups = [
     ...groupBase,
   },
 ];
+function insertGood(config, profileName) {
+  const proxyCount = config?.proxies?.length ?? 0;
+  if (proxyCount > 0) {
+    config.proxies = [...config["my-local-proxies"], ...config.proxies];
+  } else {
+    config.proxies = config["my-local-proxies"];
+  }
+  return config;
+}
 function main(config, profileName) {
+  console.log("load: " + profileName);
   const proxyCount = config?.proxies?.length ?? 0;
   const proxyProviderCount =
     typeof config?.["proxy-providers"] === "object"
       ? Object.keys(config["proxy-providers"]).length
       : 0;
+  console.log(
+    "proxies: " + proxyCount + ", proxy-providers: " + proxyProviderCount
+  );
   if (proxyCount > 0) {
     subGroup.proxies = config.proxies
       .map((proxy) => proxy.name)
@@ -67,5 +80,5 @@ function main(config, profileName) {
       obj[key] = config["rule-providers"][key];
       return obj;
     }, {});
-  return config;
+  return insertGood(config, profileName);
 }
