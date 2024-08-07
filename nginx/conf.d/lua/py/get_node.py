3c3d0a2c45ec5f6c3a5c207862561d92f8d8aad9
David Xiang <admin@mdavid.cn>
2024-08-08 01:22:42 +0800

    upt

--- nginx/conf.d/lua/py/get_node.py
+++ nginx/conf.d/lua/py/get_node.py
@@ -4,12 +4,13 @@ import uuid
 import json
 
 
-def get_node():
-    """
-    逆向某网页接口(机场节点)
-    https://linux.do/t/topic/156975
-    :return:
-    """
+def transfer2clash(data):
+    url = f'https://sub-back.zeze.love/sub?target=clash&url={data}'
+    response = requests.get(url)
+    return response.text
+
+
+def get_url():
     api = "http://bailao.cangqion.sbs/get_free_node"
     response = requests.get(api, timeout=60)
     if response.status_code != 200:
@@ -19,9 +20,22 @@ def get_node():
     raw = str(data['output'])
     s = raw.index('http')
     e = raw.index(r"']")
-    url = raw[s:e]
+    url = raw[s:e]  # 机场url
+    return url
+
+
+def get_node():
+    """
+    逆向某网页接口(机场节点)
+    https://linux.do/t/topic/156975
+    :return:
+    """
+    url = get_url()
     response = requests.get(url)
-    print(response.text)
+    data = response.text
+    data = base64.b64decode(data).decode('utf-8')
+    data = transfer2clash(data)
+    print(data)
 
 
 if __name__ == '__main__':
